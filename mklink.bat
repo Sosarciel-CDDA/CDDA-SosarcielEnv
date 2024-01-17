b299a16e38143f02ae861a28b1da48dc23e606df
zwa73 <ssfen2419@126.com>
2024-01-17 20:36:48 +0800

    upg

---
+++ mklink.bat
@@ -0,0 +1,3 @@
+mklink /D "CDDA-CustomNPC\node_modules\cdda-event" "CDDA-Event"
+mklink /D "CDDA-CustomNPC\node_modules\cdda-schema" "CDDA-Schema"
+mklink /D "CDDA-Event\node_modules\cdda-schema" "CDDA-Schema"
